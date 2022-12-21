a8700d7f6d9c95971314aa4a27fe4f5d56f94608
Albatross-hack <53970598+Albatross-hack@users.noreply.github.com>
2022-12-21 00:50:01 +0000

    reciever-code added

---
+++ Esp8266_Code/esp_sendercode.ino
@@ -0,0 +1,84 @@
+
+
+#include <ESP8266WiFi.h>
+#include <espnow.h>
+
+// REPLACE WITH THE RECEIVER'S MAC Address 40:91:51:50:63:5F
+uint8_t broadcastAddress[] = {0x40, 0x91, 0x51, 0x50, 0x63, 0x5F};
+const int buttonPin = 4;
+// Structure example to send data
+// Must match the receiver structure
+typedef struct struct_message {
+    int id; // must be unique for each sender board
+    int x;
+    int y;
+} struct_message;
+
+// Create a struct_message called myData
+struct_message myData;
+int buttonState = 0;
+// Create peer interface
+esp_now_peer_info_t peerInfo;
+
+// callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
+ 
+void setup() {
+  // Init Serial Monitor
+  Serial.begin(115200);
+  pinMode(buttonPin, INPUT);
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_register_send_cb(OnDataSent);
+  
+  // Register peer
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+  
+  // Add peer        
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+}
+ 
+void loop() {
+  // Set values to send
+  myData.id = 2;
+  myData.x = random(0,50);
+  myData.y = random(0,50);
+  buttonState = digitalRead(buttonPin);  //Takes Digital input of the Seat occupancy detector
+  if (buttonState == HIGH) {
+
+    Serial.println("HIGH");
+    delay(1000);
+  } 
+  else {
+
+    Serial.println("LOW");
+    delay(1000);
+  }
+  // Send message via ESP-NOW
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
+   
+  if (result == ESP_OK) {
+    Serial.println("Sent with success");
+  }
+  else {
+    Serial.println("Error sending the data");
+  }
+  delay(10000);
+}
